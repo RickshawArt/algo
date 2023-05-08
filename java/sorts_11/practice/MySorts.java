5c994152ffcc6f7ba09f1551af3a779071746d00
Rickshaw <892169420@qq.com>
2023-05-09 00:32:19 +0800

    [Practice] 1、插入排序的实现 2、选择排序的实现

--- java/sorts_11/practice/MySorts.java
+++ java/sorts_11/practice/MySorts.java
@@ -49,8 +49,50 @@ public class MySorts {
         if (length <= 1) {
             return;
         }
+        //i: 未排序序号，首次arr[0]作为已排序区
         for (int i = 1; i < length; i++) {
+            //未排序区中要排序的值
+            int value = arr[i];
+            //j: 已排序序号
+            int j = i - 1;
+            for (; j >= 0; j--) {
+                if (arr[j] <= value) {
+                    break;
+                }
+                arr[j + 1] = arr[j];
+            }
+            //因为判断j >= 0的时候j--了，所以要补1
+            arr[j + 1] = value;
+        }
+    }
 
+    /**
+     * 选择排序
+     * @param arr   待排序数组
+     * @author Rickshaw
+     * @since 2023/5/9 0:03
+     */
+    public static void selectionSort(int[] arr) {
+        int length = arr.length;
+        //未排序区遍历找到的最小值
+        int minIndex;
+        //用于交换数据的变量
+        int exchange;
+        //i: 已排序区序号
+        for (int i = 0; i < length - 1; i++) {
+            minIndex = i;
+            //j: 待排序区序号
+            for (int j = i + 1; j < length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            //如果当前位置就是最小值，无需进行交换
+            if (minIndex != i) {
+                exchange = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = exchange;
+            }
         }
     }
 
@@ -70,7 +112,9 @@ public class MySorts {
 
     public static void main(String[] args) {
         int[] arr = {1, 5, 4, 7, 2, 6};
-        bubbleSort(arr);
+//        bubbleSort(arr);
+//        insertionSort(arr);
+        selectionSort(arr);
         printArr(arr);
     }
 
