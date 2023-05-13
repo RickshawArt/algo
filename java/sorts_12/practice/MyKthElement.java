fb5f544a0fcebbf1d590e64f8472e300a52fe7b9
Rickshaw <892169420@qq.com>
2023-05-13 15:32:49 +0800

    [Practice] 1、O(n) 时间复杂度内求无序数组中的第 K 大元素

--- java/sorts_12/practice/MyKthElement.java
+++ java/sorts_12/practice/MyKthElement.java
@@ -19,8 +19,9 @@ public class MyKthElement {
      */
     public int getKthElement(int[] arr, int k) {
         int length = arr.length;
-        if (length < k) {
-            throw new IllegalArgumentException("The k must be greater than the array length");
+        if (k < 1 || length < k) {
+            throw new IllegalArgumentException("The k must be greater than or equal to 1 and " +
+                    "less than or equal to the array length");
         }
         return partition(arr, 0, length - 1, k);
     }
@@ -46,10 +47,15 @@ public class MyKthElement {
         }
         this.swap(arr, i, j);
         if (i + 1 > k) {
-            partition(arr, left, i - 1, k);
+            //这两处递归调用要return，才能保证找到结果得时候层层往上返回；
+            //不然找到结果返回上一层的话会继续往下执行代码，58行代码被多次执行，得到错误结果
+            return partition(arr, left, i - 1, k);
         } else if (i + 1 < k) {
-            partition(arr, i + 1, right, k);
+            //以i为分区点，左侧都比i大，右侧都比i小；
+            //数组下标从0开始，所以i + 1就是第i + 1大的元素，得出i + 1 与 k的关系
+            return partition(arr, i + 1, right, k);
         }
+        //i + 1 == k就是结果，直接层层往上返回
         return arr[i];
     }
 
@@ -73,7 +79,7 @@ public class MyKthElement {
     public static void main(String[] args) {
         MyKthElement myKthElement = new MyKthElement();
         int[] arr = {4, 2, 5, 12, 3};
-        int kthElement = myKthElement.getKthElement(arr, 2);
+        int kthElement = myKthElement.getKthElement(arr, 5);
         System.out.println("kthElement = " + kthElement);
     }
 
