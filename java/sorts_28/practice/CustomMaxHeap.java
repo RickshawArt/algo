48c1038a648808e2038af466c9a75ce31270b5cd
Rickshaw <_SSO_T_22467@csair.com>
2023-12-18 15:23:27 +0800

    [Heap] 1、自定义最大堆实现, complete

--- java/sorts_28/practice/CustomMaxHeap.java
+++ java/sorts_28/practice/CustomMaxHeap.java
@@ -2,6 +2,9 @@ package Stack.java.sorts_28.practice;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 自定义最大堆实现
@@ -38,32 +41,6 @@ public class CustomMaxHeap<E extends Comparable<E>> {
     }
 
     /**
-     * 获取泛型元素
-     * @param index    数组索引
-     * @return E
-     * @author Rickshaw
-     * @since 2023/10/25 11:38
-     */
-    @SuppressWarnings("unchecked")
-    private E elementData(int index) {
-        return (E) this.elementData[index];
-    }
-
-    /**
-     * 交换数组的两个值
-     * @param arr 数组
-     * @param i 要交换的索引
-     * @param j  要交换的索引
-     * @author Rickshaw
-     * @since 2023/10/25 11:00
-     */
-    private void swap(Object[] arr, int i, int j) {
-        Object temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    /**
      * 插入
      * @param e   要插入的元素
      * @return boolean
@@ -91,49 +68,149 @@ public class CustomMaxHeap<E extends Comparable<E>> {
 
     /**
      * 移除最大的元素
+     * @return E
      * @author Rickshaw
      * @since 2023/10/25 11:50
      */
-    public void removeMax() {
+    public E removeMax() {
         if (this.size == 0) {
-            return;
+            return null;
         }
         int i = 1;
+        E ret = this.elementData(i);
         //把根节点换成最新节点, 删除最新节点
         this.elementData[i] = this.elementData(this.size--);
         //往下堆化
+        heapify(i, this.size);
+        return ret;
+    }
+
+    /**
+     * 堆排序
+     * @param e  要转换成的泛型数组
+     * @return E[]
+     * @author Rickshaw
+     * @since 2023/12/18 10:47
+     */
+    @SuppressWarnings("unchecked")
+    public E[] sort(E[] e) {
+        this.buildHeap(this.size);
+        for (int i = this.size; i > 1; i--) {
+            this.swap(this.elementData, i, 1);
+            heapify(1, i - 1);
+        }
+        return (E[]) Arrays.copyOf(this.elementData, this.size, e.getClass());
+    }
+
+    /**
+     * 从上往下建堆
+     * @param size   建堆的范围大小
+     * @author Rickshaw
+     * @since 2023/12/18 10:14
+     */
+    public void buildHeap(int size) {
+        int heapIndex = size / 2;
+        for (int i = heapIndex; i >= 1; i--) {
+            this.heapify(i, size);
+        }
+    }
+
+    /**
+     * 堆化, 保持堆顶是最值
+     * @param index   索引
+     * @param size   堆化的范围大小
+     * @author Rickshaw
+     * @since 2023/12/15 17:16
+     */
+    private void heapify(int index, int size) {
         while (true) {
-            int leftNodeIndex = 2 * i;
-            int rightNodeIndex = 2 * i + 1;
-            //比较当前节点与其左右子节点大小, 把最大的节点作为父节点
-            int curMaxIndex = i;
-            if (this.size >= leftNodeIndex && this.elementData(curMaxIndex).compareTo(this.elementData(leftNodeIndex))  < 0) {
-                curMaxIndex = leftNodeIndex;
-            }
-            if (this.size >= rightNodeIndex && this.elementData(curMaxIndex).compareTo(this.elementData(rightNodeIndex))  < 0) {
-                curMaxIndex = rightNodeIndex;
-            }
+            int curMaxIndex = this.getCurMaxIndex(index, size);
             //父节点都比其左右子节点大, 堆化结束
-            if (i == curMaxIndex) {
+            if (index == curMaxIndex) {
                 break;
             }
-            this.swap(this.elementData, i, curMaxIndex);
-            i = curMaxIndex;
+            this.swap(this.elementData, index, curMaxIndex);
+            index = curMaxIndex;
         }
+    }
+
+    /**
+     * 从该索引节点的左右子树获取最大节点的索引
+     * @param i   索引
+     * @param size   比较的范围大小
+     * @return int
+     * @author Rickshaw
+     * @since 2023/12/15 15:03
+     */
+    private int getCurMaxIndex(int i, int size) {
+        int leftNodeIndex = 2 * i;
+        int rightNodeIndex = 2 * i + 1;
+        //比较当前节点与其左右子节点大小, 把最大的节点作为父节点
+        int curMaxIndex = i;
+        if (size >= leftNodeIndex && this.elementData(curMaxIndex).compareTo(this.elementData(leftNodeIndex))  < 0) {
+            curMaxIndex = leftNodeIndex;
+        }
+        if (size >= rightNodeIndex && this.elementData(curMaxIndex).compareTo(this.elementData(rightNodeIndex))  < 0) {
+            curMaxIndex = rightNodeIndex;
+        }
+        return curMaxIndex;
+    }
+
+    /**
+     * 获取泛型元素
+     * @param index    数组索引
+     * @return E
+     * @author Rickshaw
+     * @since 2023/10/25 11:38
+     */
+    @SuppressWarnings("unchecked")
+    private E elementData(int index) {
+        return (E) this.elementData[index];
+    }
+
+    /**
+     * 交换数组的两个值
+     * @param arr 数组
+     * @param i 要交换的索引
+     * @param j  要交换的索引
+     * @author Rickshaw
+     * @since 2023/10/25 11:00
+     */
+    private void swap(Object[] arr, int i, int j) {
+        Object temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     public static void main(String[] args) {
         CustomMaxHeap<Integer> customMaxHeap = new CustomMaxHeap<>();
-        customMaxHeap.add(8);
-        customMaxHeap.add(2);
-        customMaxHeap.add(10);
-        customMaxHeap.add(5);
-        customMaxHeap.add(12);
-        customMaxHeap.add(9);
-        customMaxHeap.add(0);
-        customMaxHeap.add(6);
-        customMaxHeap.removeMax();
-        System.out.println("customMaxHeap.elementData = " + Arrays.toString(customMaxHeap.elementData));
+//        customMaxHeap.add(8);
+//        customMaxHeap.add(2);
+//        customMaxHeap.add(10);
+//        customMaxHeap.add(5);
+//        customMaxHeap.add(12);
+//        customMaxHeap.add(9);
+//        customMaxHeap.add(0);
+//        customMaxHeap.add(6);
+//        System.out.println("customMaxHeap.elementData = " + Arrays.toString(customMaxHeap.elementData));
+//        System.out.println("customMaxHeap.removeMax() = " + customMaxHeap.removeMax());
+//        System.out.println("customMaxHeap.elementData = " + Arrays.toString(customMaxHeap.elementData));
+
+        customMaxHeap.size = 0;
+        for (int i = 1; i < customMaxHeap.elementData.length; i++) {
+            customMaxHeap.size++;
+            int random = new Random().nextInt(50);
+            customMaxHeap.elementData[i] = random;
+            if (i == 9) {
+                break;
+            }
+        }
+        Set<Object> collect = Arrays.stream(customMaxHeap.elementData).collect(Collectors.toSet());
+        if (collect.size() != customMaxHeap.size + 1) {
+            return;
+        }
+        Integer[] sort = customMaxHeap.sort(new Integer[0]);
+        System.out.println("sort = " + Arrays.toString(sort));
     }
 
 }
