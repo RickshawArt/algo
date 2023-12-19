3d47134413b4065e8223b81597df2be885f1db3d
Rickshaw <_SSO_T_22467@csair.com>
2023-12-19 15:40:28 +0800

    [Heap] 1、优化自定义堆, 可构造最大堆或最小堆, complete

--- java/sorts_28/practice/CustomHeap.java
+++ java/sorts_28/practice/CustomHeap.java
@@ -1,19 +1,16 @@
 package Stack.java.sorts_28.practice;
 
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * 自定义最大堆实现
+ * 自定义的堆实现
  *
  * @author Rickshaw
  * @version 1.0
  * @since 2023/10/25 10:23
  */
-public class CustomMaxHeap<E extends Comparable<E>> {
+public class CustomHeap<E extends Comparable<E>> implements Comparator<E> {
 
     /**
      * 存放数据的容器, 从index = 1开始存放
@@ -26,18 +23,42 @@ public class CustomMaxHeap<E extends Comparable<E>> {
     private int size;
 
     /**
+     * 是否最大堆, 默认为true, false为最小堆
+     */
+    private final boolean max;
+
+    /**
      * 默认容量, 刚好属于满二叉树
      */
     private static final int DEFAULT_CAPACITY = (1 << 4) - 1;
 
-    public CustomMaxHeap() {
+    public CustomHeap() {
         this.elementData = new Object[DEFAULT_CAPACITY + 1];
         this.size = 0;
+        this.max = true;
     }
 
-    public CustomMaxHeap(int capacity) {
+    public CustomHeap(int capacity, boolean max) {
         this.elementData = new Object[capacity + 1];
         this.size = 0;
+        this.max = max;
+    }
+
+    public CustomHeap(int capacity) {
+        this.elementData = new Object[capacity + 1];
+        this.size = 0;
+        this.max = true;
+    }
+
+    public CustomHeap(boolean max) {
+        this.elementData = new Object[DEFAULT_CAPACITY + 1];
+        this.size = 0;
+        this.max = max;
+    }
+
+    @Override
+    public int compare(E o1, E o2) {
+        return this.max ? o1.compareTo(o2) : o2.compareTo(o1);
     }
 
     /**
@@ -58,7 +79,7 @@ public class CustomMaxHeap<E extends Comparable<E>> {
         int i = this.size;
         int parentIndex = i / 2;
         //往上堆化
-        while (parentIndex > 0 && this.elementData(i).compareTo(this.elementData(parentIndex)) > 0) {
+        while (parentIndex > 0 && this.compare(this.elementData(i), this.elementData(parentIndex)) > 0) {
             this.swap(this.elementData, i, parentIndex);
             i = parentIndex;
             parentIndex = i / 2;
@@ -67,12 +88,12 @@ public class CustomMaxHeap<E extends Comparable<E>> {
     }
 
     /**
-     * 移除最大的元素
+     * 移除堆顶的元素
      * @return E
      * @author Rickshaw
      * @since 2023/10/25 11:50
      */
-    public E removeMax() {
+    public E remove() {
         if (this.size == 0) {
             return null;
         }
@@ -147,10 +168,10 @@ public class CustomMaxHeap<E extends Comparable<E>> {
         int rightNodeIndex = 2 * i + 1;
         //比较当前节点与其左右子节点大小, 把最大的节点作为父节点
         int curMaxIndex = i;
-        if (size >= leftNodeIndex && this.elementData(curMaxIndex).compareTo(this.elementData(leftNodeIndex))  < 0) {
+        if (size >= leftNodeIndex && this.compare(this.elementData(curMaxIndex), this.elementData(leftNodeIndex)) < 0) {
             curMaxIndex = leftNodeIndex;
         }
-        if (size >= rightNodeIndex && this.elementData(curMaxIndex).compareTo(this.elementData(rightNodeIndex))  < 0) {
+        if (size >= rightNodeIndex && this.compare(this.elementData(curMaxIndex), this.elementData(rightNodeIndex)) < 0) {
             curMaxIndex = rightNodeIndex;
         }
         return curMaxIndex;
@@ -182,19 +203,37 @@ public class CustomMaxHeap<E extends Comparable<E>> {
         arr[j] = temp;
     }
 
+    /**
+     * 打印数组
+     * @author Rickshaw
+     * @since 2023/12/19 14:48
+     */
+    private void printAll() {
+        System.out.print("customHeap: ");
+        for (int i = 1; i <= this.size; i++) {
+            System.out.print(this.elementData(i) + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        CustomMaxHeap<Integer> customMaxHeap = new CustomMaxHeap<>();
-//        customMaxHeap.add(8);
-//        customMaxHeap.add(2);
-//        customMaxHeap.add(10);
-//        customMaxHeap.add(5);
-//        customMaxHeap.add(12);
-//        customMaxHeap.add(9);
-//        customMaxHeap.add(0);
-//        customMaxHeap.add(6);
-//        System.out.println("customMaxHeap.elementData = " + Arrays.toString(customMaxHeap.elementData));
-//        System.out.println("customMaxHeap.removeMax() = " + customMaxHeap.removeMax());
-//        System.out.println("customMaxHeap.elementData = " + Arrays.toString(customMaxHeap.elementData));
+        CustomHeap<Integer> customMaxHeap = new CustomHeap<>(false);
+        customMaxHeap.add(8);
+        customMaxHeap.add(2);
+        customMaxHeap.add(10);
+        customMaxHeap.add(5);
+        customMaxHeap.add(12);
+        customMaxHeap.add(9);
+        customMaxHeap.add(0);
+        customMaxHeap.add(6);
+
+        customMaxHeap.printAll();
+        System.out.println("customMaxHeap.remove() = " + customMaxHeap.remove());
+        customMaxHeap.printAll();
+        System.out.println("customMaxHeap.remove() = " + customMaxHeap.remove());
+        customMaxHeap.printAll();
+        Integer[] sort = customMaxHeap.sort(new Integer[0]);
+        customMaxHeap.printAll();
 
         customMaxHeap.size = 0;
         for (int i = 1; i < customMaxHeap.elementData.length; i++) {
@@ -209,8 +248,8 @@ public class CustomMaxHeap<E extends Comparable<E>> {
         if (collect.size() != customMaxHeap.size + 1) {
             return;
         }
-        Integer[] sort = customMaxHeap.sort(new Integer[0]);
-        System.out.println("sort = " + Arrays.toString(sort));
+        customMaxHeap.printAll();
+        sort = customMaxHeap.sort(new Integer[0]);
+        customMaxHeap.printAll();
     }
-
 }
