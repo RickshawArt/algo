80a0addce785ed3757636ecc149e434fd01ea356
Rickshaw <_SSO_T_22467@csair.com>
2024-03-27 16:07:14 +0800

    1. 利用栈实现括号匹配，优化

--- java/stack_08/practice/ParenthesesMatch.java
+++ java/stack_08/practice/ParenthesesMatch.java
@@ -1,9 +1,6 @@
 package Stack.java.stack_08.practice;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -46,12 +43,21 @@ public class ParenthesesMatch {
      * @since 2023/4/26 9:34
      */
     public boolean isLegal(String expression) {
+        if (Objects.isNull(expression) || expression.isEmpty()) {
+            return false;
+        }
+        if (expression.length() % 2 == 1) {
+            return false;
+        }
         List<String> parenthesesList = this.getParenthesesList(expression);
         for (String parentheses : parenthesesList) {
             if (PARENTHESES_MAP.containsKey(parentheses)) {
                 //匹配到左括号，则放入栈
                 this.leftParenthesesStack.push(parentheses);
-            } else if (!leftParenthesesStack.isEmpty()) {
+            } else {
+                if (leftParenthesesStack.isEmpty()) {
+                    return false;
+                }
                 //匹配到右括号，则取出栈顶元素进行比较
                 String roof = this.leftParenthesesStack.pop();
                 if (!parentheses.equals(PARENTHESES_MAP.get(roof))) {
@@ -83,8 +89,8 @@ public class ParenthesesMatch {
         ParenthesesMatch parenthesesMatch = new ParenthesesMatch();
 //        System.out.println("parenthesesMatch.isLegal(\"(gs)5{sdag}][\") = " + parenthesesMatch.isLegal("(gs)5{sdag}]["));
 //        System.out.println("parenthesesMatch.isLegal(\"{[] ()[{}]}\") = " + parenthesesMatch.isLegal("{[] ()[{}]}"));
-//        System.out.println("parenthesesMatch.isLegal(\"[{()}([])]\") = " + parenthesesMatch.isLegal("[{()}([])]"));
+        System.out.println("parenthesesMatch.isLegal(\"[{()}([])]\") = " + parenthesesMatch.isLegal("[{()}([])]"));
 //        System.out.println("parenthesesMatch.isLegal(\"{[}()]\") = " + parenthesesMatch.isLegal("{[}()]"));
-        System.out.println("parenthesesMatch.isLegal(\"[({)]\") = " + parenthesesMatch.isLegal("[({)]"));
+//        System.out.println("parenthesesMatch.isLegal(\"[({)]\") = " + parenthesesMatch.isLegal("[({)]"));
     }
 }
